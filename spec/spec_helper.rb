94483217d483d9560ad8df75c38a577c87c148d1
RichardWatkins1 <richardwatkins1@gmail.com>
2016-07-22 22:23:11 +0100

    coveralls, travis added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,10 +11,11 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
-#
+require 'coveralls'
+Coveralls.wear!
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
