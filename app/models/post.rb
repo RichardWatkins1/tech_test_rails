027a834566400170321f107b39a86134bb4d2234
RichardWatkins1 <richardwatkins1@gmail.com>
2016-07-23 16:19:30 +0100

    navbar added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :image, presence: true
+
+  has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
--- spec/features/posts/creating_posts_spec.rb
+++ spec/features/posts/creating_posts_spec.rb
@@ -5,7 +5,7 @@ feature 'creating posts' do
     scenario 'should display a prompt to add a post as no posts added yet' do
       visit '/posts'
       expect(page).to have_content 'No posts yet'
-      expect(page).to have_link 'Add a blog post'
+      expect(page).to have_link 'new post'
     end
   end
 end
@@ -14,7 +14,6 @@ context 'posts have been added' do
   before do
     Post.create(title: 'My first blog post')
   end
-
   scenario 'display posts' do
     visit '/posts'
     expect(page).to have_content('My first blog post')
@@ -25,11 +24,13 @@ end
 context 'creating posts' do
   scenario 'prompts user to fill out a form, then displays the new post' do
     visit '/posts'
-    click_link 'Add a blog post'
+    click_link 'new post'
+    attach_file('Image', "spec/files/images/blog.jpg")
     fill_in 'Title', with: 'My first blog post'
     fill_in 'Body', with: 'My first blog post is rather sort although it will take much longer text'
     click_button 'Create Post'
     expect(page).to have_content 'My first blog post'
     expect(current_path).to eq '/posts'
+     expect(page).to have_css("img[src*='blog.jpg']")
   end
 end
