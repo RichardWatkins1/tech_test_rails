a3b7ef589176d9a3cde83f847bb820a3400958ad
RichardWatkins1 <richardwatkins1@gmail.com>
2016-07-26 21:59:46 +0100

    create_post deleted in web_helper to increase coveralls

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,13 +1,5 @@
 require 'fileutils'
 
-def create_post
-    click_link 'new blog post'
-    attach_file('Image', "spec/files/images/blog.jpg")
-    fill_in 'Title', with: 'My first blog post'
-    fill_in 'Body', with: 'My first blog post is rather sort although it will take much longer text'
-    click_button 'Create Post'
-end
-
 def sign_in_and_create_post
   visit '/'
   click_link 'Register'
