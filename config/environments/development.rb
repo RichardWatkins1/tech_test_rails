0a64f60f30b9cdf7c696c2e1aeb62753a9999f25
RichardWatkins1 <richardwatkins1@gmail.com>
2016-07-27 14:56:05 +0100

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "richardwatkins1",
-    password: "0545435ConSpiracy%12"
+    user_name: "your_gmail_username",
+    password: "your_gmail_password"
   }
 
   config.paperclip_defaults = {
